c1e90bed81778cefde470ebc798a449b4892cf18
Delta <ti22mau@gmail.com>
2022-01-15 00:59:46 -0300

    Finally adjusts.

--- src/main/java/com/digitalinnovation/personapi/service/PersonService.java
+++ src/main/java/com/digitalinnovation/personapi/service/PersonService.java
@@ -6,6 +6,7 @@ import com.digitalinnovation.personapi.dto.response.MessageResponseDTO;
 import com.digitalinnovation.personapi.entity.Person;
 import com.digitalinnovation.personapi.mapper.PersonMapper;
 import com.digitalinnovation.personapi.repository.PersonRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,16 +15,13 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
-
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class PersonService {
     private final PersonRepository personrepository;
 
     private final PersonMapper personMapper = PersonMapper.INSTANCE;
 
-@Autowired
-    public PersonService(PersonRepository personrepository) {
-        this.personrepository = personrepository;
-    }
+
     public MessageResponseDTO createPerson( PersonDTO personDTO) {
         Person personToSave = personMapper.toModel(personDTO);
 
